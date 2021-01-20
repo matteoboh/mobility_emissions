b8ea81b4b5ec5abf9ea7f157659cc1c6ed68484d
matteoboh <matteo.bohm@gmail.com>
2021-01-20 18:25:03 +0100

    fixed version of filter_on_time_interval

--- mobair/filtering.py
+++ mobair/filtering.py
@@ -37,48 +37,46 @@ def filter_on_time_interval(tdf, max_time=30):
 	if utils.is_multi_user(tdf):
 		if len(set(tdf[constants.UID])) != 1:
 			groupby.append(constants.UID)
-	if utils.is_multi_trajectory(tdf):
-		if len(set(tdf[constants.TID])) != 1:
-			groupby.append(constants.TID)
 
 	if len(groupby) > 0:
-		tdf_filtered = tdf.groupby(groupby, group_keys=False, as_index=False).apply(
-			filter_on_time_interval_for_1uid_1tid,
-			max_time)
+		tdf_filtered = tdf.groupby(groupby, group_keys=False, as_index=False).apply(filter_on_time_interval_for_1uid,
+																					max_time)
 	else:
-		tdf_filtered = filter_on_time_interval_for_1uid_1tid(tdf, max_time)
+		tdf_filtered = filter_on_time_interval_for_1uid(tdf, max_time)
 
 	return tdf_filtered.sort_by_uid_and_datetime().reset_index(drop=True)
 
 
-def filter_on_time_interval_for_1uid_1tid(tdf, max_time=30):
-	tdf_1uid_1tid = tdf.copy().reset_index(drop=True)
+def filter_on_time_interval_for_1uid(tdf, max_time=30):
+	tdf_1uid = tdf.copy().reset_index(drop=True)
 
 	# creating DF with time differences between consecutive points
-	df_test = pd.DataFrame(tdf_1uid_1tid.datetime.diff())
+	df_timediffs = pd.DataFrame(tdf_1uid.datetime.diff())
 
 	# Selecting the sub-trajectories, composed by points that are selected s.t.
 	# an index of a point pi is saved if:
 	# dist(pi, pi-1) < max_time OR
 	# dist(pi+1, pi) < max_time
-	list_indexes = [index for index in df_test.index[:-1] if (df_test.loc[index].datetime < timedelta(seconds=120)) or (
-				df_test.loc[index + 1].datetime < timedelta(seconds=120))]
-	last_index = df_test.index[-1]
-	if df_test.loc[last_index].datetime < timedelta(seconds=120):
+	list_indexes = [index for index in df_timediffs.index[:-1] if
+					(df_timediffs.loc[index].datetime < timedelta(seconds=max_time)) or (
+								df_timediffs.loc[index + 1].datetime < timedelta(seconds=max_time))]
+	last_index = df_timediffs.index[-1]
+	if df_timediffs.loc[last_index].datetime < timedelta(seconds=max_time):
 		list_indexes.extend([last_index])
-	tdf_1uid_1tid__filt = tdf_1uid_1tid.loc[list_indexes]
-	tdf_1uid_1tid__filt.reset_index(inplace=True)
+	tdf_1uid__filt = tdf_1uid.loc[list_indexes]
+	tdf_1uid__filt.reset_index(inplace=True)
 
 	# Creating the new tid for the selected sub-trajectories
-	i = 1
-	for index in tdf_1uid_1tid__filt.index[1:]:
-		c_old_index = tdf_1uid_1tid__filt.loc[index]['index']
-		previous_old_index = tdf_1uid_1tid__filt.loc[index - 1]['index']
+	for index in tdf_1uid__filt.index[1:]:
+		c_old_index = tdf_1uid__filt.loc[index]['index']
+		previous_old_index = tdf_1uid__filt.loc[index - 1]['index']
 		if c_old_index - previous_old_index > 1:
-			tdf_1uid_1tid__filt.loc[index:, 'tid'] = tdf_1uid_1tid__filt.loc[index - 1, 'tid'] + i
-			i += 1
+			tdf_1uid__filt.loc[index:, 'tid'] = tdf_1uid__filt.loc[index - 1, 'tid'] + 1
+		else:
+			if df_timediffs.loc[c_old_index].datetime > timedelta(seconds=max_time):
+				tdf_1uid__filt.loc[index:, 'tid'] = tdf_1uid__filt.loc[index - 1, 'tid'] + 1
 
-	# Selecting only sub-trajectories composed by more than one points
-	tdf_1uid_1tid__filt__final = tdf_1uid_1tid__filt.groupby('tid').filter(lambda x: len(x) > 1).drop('index', axis=1)
+	# Selecting only sub-trajectories composed by more than one point
+	tdf_1uid__filt__final = tdf_1uid__filt.groupby('tid').filter(lambda x: len(x) > 1).drop('index', axis=1)
 
-	return tdf_1uid_1tid__filt__final
+	return tdf_1uid__filt__final
