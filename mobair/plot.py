faffac07dcce5d9d1ccea51a0fc4caa938ac7099
matteoboh <bohm@diag.uniroma1.it>
2022-02-17 10:51:17 +0100

    new version of streetDraw

--- mobair/plot.py
+++ mobair/plot.py
@@ -2,7 +2,8 @@ import osmnx as ox
 import altair as alt
 
 
-def streetDraw(place, G, attribute_to_plot, schemeColor='yelloworangered', save_fig=False):
+def streetDraw(place, G, attribute_to_plot, normalize_by_length=False, show_legend=True, schemeColor='yelloworangered',
+               size=(600, 600), save_fig=False):
     """ Plots a road network with the roads colored with respect to an attribute.
 
     Parameters
@@ -16,11 +17,21 @@ def streetDraw(place, G, attribute_to_plot, schemeColor='yelloworangered', save_
     attribute_to_plot : str
         the name of the feature to be plotted. It must be present as an attribute of the graph's edges.
 
+    normalize_by_length : boolean
+        whether to normalize the value of the attribute with the edge's length.
+
+    show_legend : boolean
+        whether to show the legend (colorbar) or not.
+
     schemeColor : str
         the name of a Vega's color scheme (https://vega.github.io/vega/docs/schemes/)
 
+    size : tuple
+        the size of the chart as (widthCanvas, heightCanvas).
+
     save_fig
-        whether to save (in .svg and .png) the resulting figure or not.
+        whether to save (in .svg and .png) the output chart or not.
+
     Returns
         the Altair chart.
     -------
@@ -28,12 +39,28 @@ def streetDraw(place, G, attribute_to_plot, schemeColor='yelloworangered', save_
     Credits to Daniele Fadda.
     """
 
-    widthCanvas = 600
-    heightCanvas = 600
+    widthCanvas = size[0]
+    heightCanvas = size[1]
 
     nodes, edges = ox.graph_to_gdfs(G)
     df = edges.reset_index()
     df.sort_values(by=attribute_to_plot, na_position='first', ascending=True, inplace=True)
+
+    if normalize_by_length:
+        attribute_to_plot_old = attribute_to_plot
+        attribute_to_plot = attribute_to_plot + ' normalized'
+        df[attribute_to_plot] = df[attribute_to_plot_old] / df['length']
+        plot_label = '%s (grams per meter of road)' % attribute_to_plot_old
+    else:
+        plot_label = '%s' % attribute_to_plot
+    #
+    first_nonzero = [x for x in df[attribute_to_plot].dropna().sort_values() if x != 0][0]
+    #
+    if show_legend:
+        legend = alt.Legend(title=plot_label)
+    else:
+        legend = None
+    #
 
     lines = alt.Chart(df[df[attribute_to_plot].isnull()]).mark_geoshape(
         filled=False,
@@ -47,22 +74,22 @@ def streetDraw(place, G, attribute_to_plot, schemeColor='yelloworangered', save_
     ).encode(
         alt.Color(
             attribute_to_plot + ':Q',
-            # legend=None,
-            scale=alt.Scale(scheme=schemeColor, type='symlog')
+            legend=legend,
+            scale=alt.Scale(scheme=schemeColor, type='symlog', constant=first_nonzero)
         )
     ).properties(
         width=widthCanvas,
         height=heightCanvas
     )
-    # water = waterDraw(place)
 
-    chart = lines + speedLines  # + water
+    chart = lines + speedLines
 
     if save_fig:
         from altair_saver import save
         city = place.split(',')[0]
-        save(chart, f'{city}_map_{attribute_to_plot}.png')
-        save(chart, f'{city}_map_{attribute_to_plot}.svg')
+        attribute = attribute_to_plot.replace(' ', '_')
+        save(chart, f'{city}_map_{attribute}.png')
+        save(chart, f'{city}_map_{attribute}.svg')
         print(f'{city} images saved')
 
     return chart
